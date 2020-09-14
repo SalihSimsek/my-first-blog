2bd1dca34de5c4444a95e7003a74cf21c94d08d6
salihsimseek@gmail.com <salihsimseek@gmail.com>
2020-09-14 15:58:49 +0300

    ekleme ve editleme alanları eklendi

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from .views import *
 app_name="post"
 urlpatterns = [
     path('',post_list,name='post_list'),
-    path('<int:pk>/detail/',post_detail,name='post_detail')
+    path('post/<int:pk>/',post_detail,name='post_detail'),
+    path('post/create/',post_new,name='post_new'),
+    path('post/edit/<int:pk>/',post_edit,name='post_edit'),
 ]
